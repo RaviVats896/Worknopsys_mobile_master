a44e6a536dc926c47559e39097ee2b4b1c7229aa
Ravi Vats <ravi.vats496@gmail.com>
2016-12-26 01:38:50 +0530

    First Commit

---
+++ app/src/main/java/com/example/ravivats/worknopsysmobile/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.ravivats.worknopsysmobile;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
