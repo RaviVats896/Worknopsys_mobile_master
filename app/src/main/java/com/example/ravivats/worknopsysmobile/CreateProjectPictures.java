953b8e80720b5ee14ec91205c16e4986155dbec8
Ravi Vats <ravi.vats496@gmail.com>
2016-12-29 00:13:22 +0530

    Create Project Layout Done.

--- app/src/main/java/com/example/ravivats/worknopsysmobile/CreateProjectPictures.java
+++ app/src/main/java/com/example/ravivats/worknopsysmobile/CreateProjectPictures.java
@@ -1,40 +1,27 @@
 package com.example.ravivats.worknopsysmobile;
 
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
+import android.view.View;
+import android.widget.Button;
 
-public class Main2Activity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
-    TextView navDrawerNumber;
+public class CreateProjectPictures extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    Button cpPicturesNxtBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_create_project_pictures);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -42,6 +29,14 @@ public class Main2Activity extends AppCompatActivity
         toggle.syncState();
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        cpPicturesNxtBtn = (Button) findViewById(R.id.cp_pictures_nextBtn);
+        cpPicturesNxtBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(CreateProjectPictures.this, CreateProjectOrders.class);
+                startActivity(in);
+            }
+        });
     }
 
     @Override
@@ -52,30 +47,6 @@ public class Main2Activity extends AppCompatActivity
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main2, menu);
-        navDrawerNumber = (TextView) findViewById(R.id.navDrawertxtViewNumber);
-        navDrawerNumber.setText(Constants.getPhoneNumber());
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
@@ -91,8 +62,8 @@ public class Main2Activity extends AppCompatActivity
         } else if (id == R.id.nav_create_customer) {
 
         } else if (id == R.id.nav_create_project) {
-            Intent i = new Intent(Main2Activity.this, CreateProjectDetails.class);
-            startActivity(i);
+            //Intent i=new Intent(CreateProject.this,CreateProject.class);
+            //startActivity(i);
 
         } else if (id == R.id.nav_mgmt_working_orders) {
 
