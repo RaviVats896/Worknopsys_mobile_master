4a29c03f801f8666f75eb3ad32e73a4b3f7710b9
Ravi Vats <ravi.vats496@gmail.com>
2016-12-29 01:27:11 +0530

    Create Project part

--- app/src/main/java/com/example/ravivats/worknopsysmobile/CreateProjectOrders.java
+++ app/src/main/java/com/example/ravivats/worknopsysmobile/CreateProjectOrders.java
@@ -14,6 +14,7 @@ import android.widget.Button;
 
 public class CreateProjectOrders extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     Button cpOrdersNxtBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,6 +36,7 @@ public class CreateProjectOrders extends AppCompatActivity implements Navigation
                 //startActivity(in);
             }
         });
+
     }
 
     @Override
--- app/src/main/java/com/example/ravivats/worknopsysmobile/CreateProjectPictures.java
+++ app/src/main/java/com/example/ravivats/worknopsysmobile/CreateProjectPictures.java
@@ -1,6 +1,13 @@
 package com.example.ravivats.worknopsysmobile;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -8,18 +15,34 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class CreateProjectPictures extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     Button cpPicturesNxtBtn;
+    ImageButton cpPicturesCameraBtn, cpPicturesUploadBtn, cpPicturesDeleteBtn;
+    String userChoosenTask;
+    ImageView cpPicturesImageView;
+    private int REQUEST_CAMERA = 2;
+    private int SELECT_FILE = 1;
+    private int flag;
+    private static final String TAG = "AddBook";
+    private Uri fileUri = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_project_pictures);
-
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -29,7 +52,24 @@ public class CreateProjectPictures extends AppCompatActivity implements Navigati
         toggle.syncState();
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        cpPicturesImageView = (ImageView) findViewById(R.id.cp_pictures_ImageView);
         cpPicturesNxtBtn = (Button) findViewById(R.id.cp_pictures_nextBtn);
+        cpPicturesCameraBtn = (ImageButton) findViewById(R.id.cp_pictures_camera);
+        cpPicturesUploadBtn = (ImageButton) findViewById(R.id.cp_pictures_upload);
+        cpPicturesDeleteBtn = (ImageButton) findViewById(R.id.cp_pictures_delete);
+        cpPicturesImageView.setImageResource(R.drawable.ic_satellite_black_24dp);
+        cpPicturesCameraBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                selectImage();
+            }
+        });
+        cpPicturesDeleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cpPicturesImageView.setImageResource(R.drawable.ic_satellite_black_24dp);
+            }
+        });
         cpPicturesNxtBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,6 +77,115 @@ public class CreateProjectPictures extends AppCompatActivity implements Navigati
                 startActivity(in);
             }
         });
+
+    }
+
+    private void selectImage() {
+        final CharSequence[] items = {"Take Photo", "Choose from Library",
+                "Cancel"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(CreateProjectPictures.this);
+        builder.setTitle("Add Photo!");
+        builder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                boolean result = Utility.checkPermission(CreateProjectPictures.this);
+                if (items[item].equals("Take Photo")) {
+                    userChoosenTask = "Take Photo";
+                    if (result)
+                        cameraIntent();
+                } else if (items[item].equals("Choose from Library")) {
+                    userChoosenTask = "Choose from Library";
+                    if (result)
+                        galleryIntent();
+                } else if (items[item].equals("Cancel")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        builder.show();
+    }
+
+    private void cameraIntent() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, REQUEST_CAMERA);
+    }
+
+    private void galleryIntent() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select File"), SELECT_FILE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == SELECT_FILE) {
+                Bitmap bm = onSelectFromGalleryResult(data);
+                Log.d(TAG, "gallery");
+                cpPicturesImageView.setImageBitmap(bm);
+            } else if (requestCode == REQUEST_CAMERA) {
+                onCaptureImageResult(data);
+                Log.d(TAG, "image");
+            }
+
+        }
+    }
+
+    @SuppressWarnings("deprecation")
+    private Bitmap onSelectFromGalleryResult(Intent data) {
+        Bitmap bm = null;
+        if (data != null) {
+            Log.d(TAG, "data not null");
+            try {
+                bm = MediaStore.Images.Media.getBitmap(getApplicationContext().getContentResolver(), data.getData());
+                getUri(bm);
+
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        flag = 1;
+        return bm;
+    }
+
+
+    private void onCaptureImageResult(Intent data) {
+
+        // Bitmap bitmap = cpPicturesImageView.getDrawingCache();
+        Bitmap thumbnail = (Bitmap) data.getExtras().get("data");
+        //Bitmap b = Bitmap.createScaledBitmap(thumbnail, 150, 150, false);
+        cpPicturesImageView.setImageBitmap(thumbnail);
+        flag = 1;
+        getUri(thumbnail);
+    }
+
+    private void getUri(Bitmap bitmap) {
+        cpPicturesImageView.setDrawingCacheEnabled(true);
+        cpPicturesImageView.buildDrawingCache();
+
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 90, bytes);
+        File destination = new File(Environment.getExternalStorageDirectory(),
+                System.currentTimeMillis() + ".jpg");
+
+        Log.d(TAG, "dest" + destination);
+        FileOutputStream fo;
+        try {
+            destination.createNewFile();
+            fo = new FileOutputStream(destination);
+            fo.write(bytes.toByteArray());
+            fo.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        fileUri = Uri.fromFile(destination);
+
     }
 
     @Override
