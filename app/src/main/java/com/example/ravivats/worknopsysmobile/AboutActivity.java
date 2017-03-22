c4248f48e2830346e84d66a958885260ffddaf49
ravivats <vats.ravi96@gmail.com>
2017-03-23 01:07:00 +0530

    Authorization part UI fully done.

--- app/src/main/java/com/example/ravivats/worknopsysmobile/AboutActivity.java
+++ app/src/main/java/com/example/ravivats/worknopsysmobile/AboutActivity.java
@@ -11,7 +11,8 @@ import com.github.aakira.expandablelayout.ExpandableRelativeLayout;
 
 public class AboutActivity extends AppCompatActivity {
     Button authButton;
-    Switch custCreate,custView,projCreate,projView,woCreate,woView;
+    Switch custCreate,custView,projCreate,projView,woCreate,woView,woEdit,woDelete,authPass,authPin;
+    Switch hoursAdd, hoursEdit,hoursQRScan,woShowall,woSortDistance;
     ExpandableRelativeLayout expandableLayout1;
     Authorization userAuth;
     @Override
@@ -25,15 +26,33 @@ public class AboutActivity extends AppCompatActivity {
         projView=(Switch) findViewById(R.id.about_adm_emp_proj_view);
         woCreate=(Switch) findViewById(R.id.about_adm_emp_wo_create);
         woView=(Switch) findViewById(R.id.about_adm_emp_wo_view);
+        woEdit=(Switch) findViewById(R.id.about_adm_emp_wo_edit);
+        woDelete=(Switch) findViewById(R.id.about_adm_emp_wo_delete);
+        authPass=(Switch) findViewById(R.id.about_adm_auth_pass);
+        authPin=(Switch) findViewById(R.id.about_adm_auth_pin);
+        hoursAdd=(Switch) findViewById(R.id.about_adm_hours_add);
+        hoursEdit=(Switch) findViewById(R.id.about_adm_hours_edit);
+        hoursQRScan=(Switch) findViewById(R.id.about_adm_hours_member_QRScan);
+        woShowall=(Switch) findViewById(R.id.about_adm_wo_show_all);
+        woSortDistance=(Switch) findViewById(R.id.about_adm_wo_sort_distance);
+
         userAuth=Constants.getAUTH();
 
         if(userAuth.getAdmEmpCustCreate())  custCreate.setChecked(true);
         if(userAuth.getAdmEmpCustView())  custView.setChecked(true);
         if(userAuth.getAdmEmpProjCreate())  projCreate.setChecked(true);
         if(userAuth.getAdmEmpProjView())  projView.setChecked(true);
-        if(userAuth.getAdmWoCreate()  )  woCreate.setChecked(true);
-        if(userAuth.getAdmWoView()  )  woView.setChecked(true);
-
+        if(userAuth.getAdmWoCreate())  woCreate.setChecked(true);
+        if(userAuth.getAdmWoView())  woView.setChecked(true);
+        if(userAuth.getAdmWoEdit())  woEdit.setChecked(true);
+        if(userAuth.getAdmWoDelete())  woDelete.setChecked(true);
+        if(userAuth.getAuthPass())  authPass.setChecked(true);
+        if(userAuth.getAuthPin())  authPin.setChecked(true);
+        if(userAuth.getHoursAdd())  hoursAdd.setChecked(true);
+        if(userAuth.getHoursEdit())  hoursEdit.setChecked(true);
+        if(userAuth.getHoursMemberQRscan())  hoursQRScan.setChecked(true);
+        if(userAuth.getWoShowAll())  woShowall.setChecked(true);
+        if(userAuth.getWoSortDistance())  woSortDistance.setChecked(true);
 
         authButton.setOnClickListener(new View.OnClickListener() {
             @Override
