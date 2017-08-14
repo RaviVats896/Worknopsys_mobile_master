87ae1b90f7d003aaf5ab72a46041061472ed7052
ravivats <vats.ravi96@gmail.com>
2017-08-15 01:44:58 +0530

    edit working order layout done.

--- app/src/main/java/com/example/ravivats/worknopsysmobile/WorkingOrders/MyWorkingOrderDetails.java
+++ app/src/main/java/com/example/ravivats/worknopsysmobile/WorkingOrders/MyWorkingOrderDetails.java
@@ -1,18 +1,69 @@
 package com.example.ravivats.worknopsysmobile.WorkingOrders;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.ravivats.worknopsysmobile.Constants;
 import com.example.ravivats.worknopsysmobile.R;
+import com.example.ravivats.worknopsysmobile.domain.WorkingOrder;
+
+import java.util.ArrayList;
+import java.util.Map;
 
 public class MyWorkingOrderDetails extends AppCompatActivity {
+    ArrayList<WorkingOrder> workingOrders;
+    WorkingOrder workingOrder;
+    int workingOrderIndex;
+    TextView editWOResources, editWOCustomerName, editWOTaskName, editWOProjectName, editWOStartDate, editWOAddress;
+    Button editWOTimingsBtn, editWOResourcesBtn;
+    Map<String, String> taskInvMap, custInvMap, projectInvMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_working_order_details);
-        int position= getIntent().getIntExtra("position",0);
-        Toast.makeText(this, "This is "+position, Toast.LENGTH_SHORT).show();
+        workingOrderIndex = getIntent().getIntExtra("position", 0);
+        workingOrders = new ArrayList<WorkingOrder>();
+        workingOrders = Constants.getWorkingOrders();
+        workingOrder = workingOrders.get(workingOrderIndex);
+        taskInvMap = Constants.getTaskInvMap();
+        custInvMap = Constants.getCustomerInvMap();
+        projectInvMap = Constants.getProjectInvMap();
+
+        editWOResources = (TextView) findViewById(R.id.edit_wo_editText_resources);
+        editWOCustomerName = (TextView) findViewById(R.id.edit_wo_spinner_cID);
+        editWOTaskName = (TextView) findViewById(R.id.edit_wo_spinner_tID);
+        editWOProjectName = (TextView) findViewById(R.id.edit_wo_spinner_pID);
+        editWOStartDate = (TextView) findViewById(R.id.edit_wo_editText_sDate);
+        editWOAddress = (TextView) findViewById(R.id.edit_wo_editText_address);
+        editWOResourcesBtn = (Button) findViewById(R.id.edit_wo_resources_button);
+        editWOTimingsBtn = (Button) findViewById(R.id.edit_wo_timings_button);
+
+        editWOResources.setText("Resources: " + workingOrder.getResources());
+        editWOStartDate.setText("Date: " + workingOrder.getStartDate());
+        editWOAddress.setText("Address: "+workingOrder.getAddress());
+        editWOProjectName.setText(projectInvMap.get(workingOrder.getProject()));
+        editWOCustomerName.setText(custInvMap.get(workingOrder.getCustomer()));
+        editWOTaskName.setText(taskInvMap.get(workingOrder.getTask()));
+
+        editWOResourcesBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        editWOTimingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MyWorkingOrderDetails.this,TimingsEvidence.class));
+            }
+        });
     }
 }
