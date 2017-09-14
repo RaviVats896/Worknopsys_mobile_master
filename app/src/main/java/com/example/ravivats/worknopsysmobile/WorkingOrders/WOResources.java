47b601987e9bb8239686c25e3b699eb55c2371e1
ravivats <vats.ravi96@gmail.com>
2017-09-14 07:45:23 +0530

    resources api done

--- app/src/main/java/com/example/ravivats/worknopsysmobile/WorkingOrders/WOResources.java
+++ app/src/main/java/com/example/ravivats/worknopsysmobile/WorkingOrders/WOResources.java
@@ -2,6 +2,7 @@ package com.example.ravivats.worknopsysmobile.WorkingOrders;
 
 import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -14,15 +15,21 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 import com.example.ravivats.worknopsysmobile.Constants;
-import com.example.ravivats.worknopsysmobile.Project.CreateProjectPictures;
 import com.example.ravivats.worknopsysmobile.R;
 import com.example.ravivats.worknopsysmobile.Utility;
 import com.example.ravivats.worknopsysmobile.domain.WorkingOrder;
@@ -33,20 +40,25 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
-public class WOResources extends AppCompatActivity {
+public class WOResources extends AppCompatActivity implements DatePickerDialog.OnDateSetListener {
+    static final String RESOURCES_URL = "http://worknopsys.ml:5000/api/resourcesapp/create";
     private int REQUEST_CAMERA = 2, SELECT_FILE = 1, flag, workingOrderIndex, imageViewChoice = 0;
     private static final String TAG = "WOResources";
     private Uri fileUri;
+    Calendar cal;
+    StringRequest resourcesRequest;
+    DatePickerDialog.OnDateSetListener woResourcesWorkDateListener;
     ImageButton woResourcesCamera1Btn, woResourcesUpload1Btn, woResourcesDelete1Btn, woResourcesCamera2Btn,
             woResourcesUpload2Btn, woResourcesDelete2Btn, woResourcesCamera3Btn, woResourcesUpload3Btn, woResourcesDelete3Btn;
     ImageView woResourcesImageView1, woResourcesImageView2, woResourcesImageView3;
-    EditText woResourcesImageDesc1, woResourcesImageDesc2, woResourcesImageDesc3;
+    EditText woResourcesImageDesc1, woResourcesImageDesc2, woResourcesImageDesc3, woResourcesWorkDate, woResourcesPName;
     Button woResourcesNxtBtn;
     String userChosenTask;
-    String picLocation1= "", picLocation2= "", picLocation3 = "", picId;
+    String picLocation1 = "", picLocation2 = "", picLocation3 = "", picId;
     ArrayList<WorkingOrder> workingOrders;
     WorkingOrder workingOrder;
 
@@ -69,12 +81,17 @@ public class WOResources extends AppCompatActivity {
         woResourcesImageDesc1 = (EditText) findViewById(R.id.wo_resources_description1);
         woResourcesImageDesc2 = (EditText) findViewById(R.id.wo_resources_description2);
         woResourcesImageDesc3 = (EditText) findViewById(R.id.wo_resources_description3);
+        woResourcesWorkDate = (EditText) findViewById(R.id.wo_resources_wDate);
+        woResourcesPName = (EditText) findViewById(R.id.wo_resources_pName);
         woResourcesNxtBtn = (Button) findViewById(R.id.wo_resources_nextBtn);
 
         workingOrderIndex = getIntent().getIntExtra("woPosition", 0);
         workingOrders = new ArrayList<WorkingOrder>();
         workingOrders = Constants.getWorkingOrders();
         workingOrder = workingOrders.get(workingOrderIndex);
+
+        final RequestQueue requestQueue = Volley.newRequestQueue(this);
+        cal = Calendar.getInstance();
 
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
@@ -87,6 +104,24 @@ public class WOResources extends AppCompatActivity {
         woResourcesImageView1.setImageResource(R.drawable.ic_menu_gallery_black);
         woResourcesImageView2.setImageResource(R.drawable.ic_menu_gallery_black);
         woResourcesImageView3.setImageResource(R.drawable.ic_menu_gallery_black);
+
+        woResourcesWorkDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog dp1 = new DatePickerDialog(WOResources.this, woResourcesWorkDateListener, cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH));
+                dp1.show();
+            }
+        });
+
+        woResourcesWorkDateListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                cal.set(Calendar.YEAR, year);
+                cal.set(Calendar.MONTH, month);
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                woResourcesWorkDate.setText(dayOfMonth + "/" + (month + 1) + "/" + year);
+            }
+        };
 
         woResourcesCamera1Btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -115,7 +150,7 @@ public class WOResources extends AppCompatActivity {
                 try {
                     picId = workingOrder.getId() + Constants.getDate() + "_" + Constants.getTime();
                     Log.e("picID", picId);
-                    if(!picLocation1.equals("")) {
+                    if (!picLocation1.equals("")) {
                         Map m = cloudinary.uploader().upload(picLocation1, ObjectUtils.asMap("public_id", picId));
                         Toast.makeText(WOResources.this, "Upload Successful.", Toast.LENGTH_SHORT).show();
                     }
@@ -132,7 +167,7 @@ public class WOResources extends AppCompatActivity {
                 try {
                     picId = workingOrder.getId() + Constants.getDate() + "_" + Constants.getTime();
                     Log.e("picID", picId);
-                    if(!picLocation2.equals("")) {
+                    if (!picLocation2.equals("")) {
                         Map m = cloudinary.uploader().upload(picLocation2, ObjectUtils.asMap("public_id", picId));
                         Toast.makeText(WOResources.this, "Upload Successful.", Toast.LENGTH_SHORT).show();
                     }
@@ -149,7 +184,7 @@ public class WOResources extends AppCompatActivity {
                 try {
                     picId = workingOrder.getId() + Constants.getDate() + "_" + Constants.getTime();
                     Log.e("picID", picId);
-                    if(!picLocation3.equals("")) {
+                    if (!picLocation3.equals("")) {
                         Map m = cloudinary.uploader().upload(picLocation3, ObjectUtils.asMap("public_id", picId));
                         Toast.makeText(WOResources.this, "Upload Successful.", Toast.LENGTH_SHORT).show();
                     }
@@ -182,8 +217,47 @@ public class WOResources extends AppCompatActivity {
         woResourcesNxtBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                final String[] rList= {picLocation1, picLocation2,picLocation3};
+                String[] rDescriptionList= {woResourcesImageDesc1.getText().toString(),woResourcesImageDesc2.getText().toString(),
+                        woResourcesImageDesc3.getText().toString()};
+                resourcesRequest = new StringRequest(Request.Method.POST, RESOURCES_URL,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                Toast.makeText(WOResources.this, "Response is:" + response, Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(WOResources.this, MyWorkingOrderDetails.class));
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                Toast.makeText(WOResources.this, "Request failed! Please check your Internet Connection.", Toast.LENGTH_SHORT).show();
+                                error.printStackTrace();
+                            }
+                        }) {
+                    @Override
+                    protected Map<String, String> getParams() {
+                        Map<String, String> params = new HashMap<String, String>();
+                        params.put("employee", Constants.getEMPLOYEE().getId());
+                        params.put("wo", workingOrder.getId());
+                        params.put("startdate", workingOrder.getStartDate());
+                        params.put("workdate", woResourcesWorkDate.getText().toString());
+                        params.put("pname", woResourcesPName.getText().toString());
+                        params.put("rlist1", picLocation1);
+                        params.put("rlist2", picLocation2);
+                        params.put("rlist3", picLocation3);
+                        params.put("rdesc1", woResourcesImageDesc1.getText().toString());
+                        params.put("rdesc2", woResourcesImageDesc2.getText().toString());
+                        params.put("rdesc3", woResourcesImageDesc3.getText().toString());
+                        return params;
+                    }
+
+                };
+                requestQueue.add(resourcesRequest);
             }
         });
+
+
     }
 
     private void selectImage() {
@@ -338,5 +412,9 @@ public class WOResources extends AppCompatActivity {
             default:
                 break;
         }
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
     }
 }
