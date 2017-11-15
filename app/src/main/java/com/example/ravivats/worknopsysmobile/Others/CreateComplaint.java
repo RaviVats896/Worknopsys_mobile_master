4a07755ceec4e50ca5e876152ddb9381dda13bea
ravivats <vats.ravi96@gmail.com>
2017-11-15 09:06:30 +0530

    Complaint Layout pushed.

---
+++ app/src/main/java/com/example/ravivats/worknopsysmobile/Others/CreateComplaint.java
@@ -0,0 +1,15 @@
+package com.example.ravivats.worknopsysmobile.Others;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ravivats.worknopsysmobile.R;
+
+public class CreateComplaint extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_complaint);
+    }
+}
