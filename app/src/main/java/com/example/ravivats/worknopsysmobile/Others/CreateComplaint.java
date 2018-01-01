e5c01a6fe2d89480841e3de83c237461788f915c
ravivats <vats.ravi96@gmail.com>
2018-01-01 13:12:14 +0530

    Complaint API pushed.

--- app/src/main/java/com/example/ravivats/worknopsysmobile/Others/CreateComplaint.java
+++ app/src/main/java/com/example/ravivats/worknopsysmobile/Others/CreateComplaint.java
@@ -10,20 +10,31 @@ import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.example.ravivats.worknopsysmobile.Constants;
 import com.example.ravivats.worknopsysmobile.R;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Map;
 
 public class CreateComplaint extends AppCompatActivity {
+    static final String createComplaint_URL = "http://worknopsys.ml:5000/api/complaint/create";
     Spinner categorySpinner, noOfPeopleSpinner, facingSinceSpinner, citySpinner;
     String categoryValue, noOfPeopleValue, facingSinceValue, cityValue;
-    Map<String, String> categoryMap, noOfPeopleMap, facingSinceMap, cityMap;
-    ArrayList<String> categoryNameList, noOfPeopleNameList, facingSinceNameList, cityNameList;
+    Map<String, String> categoryMap, cityMap;
+    ArrayList<String> categoryNameList, cityNameList;
     ArrayAdapter<CharSequence> noOfPeopleAdapter, facingSinceAdapter;
     Button ccSubmitButton;
     EditText complaintTitle, complaintDesc;
+    String facingSinceArray[] = {"0–10 days", "10–30 days", "30–50 days", "More than 50 days"};
+    String noOfPeopleArray[] = {"0–10 people", "10–30 people", "30–50 people", "More than 50 people"};
+    StringRequest createComplaintRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +47,7 @@ public class CreateComplaint extends AppCompatActivity {
         ccSubmitButton = (Button) findViewById(R.id.cc_submit_button);
         complaintTitle = (EditText) findViewById(R.id.cc_title_edit_text);
         complaintDesc = (EditText) findViewById(R.id.cc_description_edit_text);
-
+        final RequestQueue requestComplaintQueue = Volley.newRequestQueue(this);
         cityMap = Constants.getCityMap();
         categoryMap = Constants.getCategoryMap();
 
@@ -69,6 +80,30 @@ public class CreateComplaint extends AppCompatActivity {
         facingSinceAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);// Apply the adapter to the spinner
         facingSinceSpinner.setAdapter(facingSinceAdapter);
 
+        facingSinceSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                facingSinceValue = facingSinceArray[position];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                facingSinceValue = facingSinceArray[0];
+            }
+        });
+
+        noOfPeopleSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                noOfPeopleValue = noOfPeopleArray[position];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                noOfPeopleValue = noOfPeopleArray[0];
+            }
+        });
+
         categorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
@@ -90,6 +125,47 @@ public class CreateComplaint extends AppCompatActivity {
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
+
+        ccSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (facingSinceValue== null || noOfPeopleValue ==null  || complaintTitle.getText().toString().equals("")|| complaintDesc.getText().toString().equals("")) {
+                    Toast.makeText(CreateComplaint.this, "Please fill all details.", Toast.LENGTH_SHORT).show();
+                } else {
+                    final String title = complaintTitle.getText().toString();
+                    final String description = complaintDesc.getText().toString();
+                    createComplaintRequest = new StringRequest(Request.Method.POST, createComplaint_URL,
+                            new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+                                    Toast.makeText(CreateComplaint.this, "Complaint submission successful!" + response, Toast.LENGTH_SHORT).show();
+                                }
+                            },
+                            new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+                                    error.printStackTrace();
+                                }
+                            }) {
+                        @Override
+                        protected Map<String, String> getParams() {
+                            Map<String, String> params = new HashMap<String, String>();
+                            params.put("workerid", Constants.getEMPLOYEE().getId());
+                            params.put("title", title);
+                            params.put("description", description);
+                            params.put("people", noOfPeopleValue);
+                            params.put("days", facingSinceValue);
+                            params.put("categoryid", categoryValue);
+                            params.put("cityid", cityValue);
+
+                            return params;
+                        }
+                    };
+
+                    requestComplaintQueue.add(createComplaintRequest);
+                }
             }
         });
     }
