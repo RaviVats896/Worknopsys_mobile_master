83f45382617cd78a863a5f4b2dc68fcf7780cd54
ravivats <vats.ravi96@gmail.com>
2018-09-02 12:40:10 +0530

    Back activity no return fixed for Login screen.

--- app/src/main/java/com/example/ravivats/worknopsysmobile/Others/LoginActivity.java
+++ app/src/main/java/com/example/ravivats/worknopsysmobile/Others/LoginActivity.java
@@ -171,4 +171,9 @@ public class LoginActivity extends AppCompatActivity {
                     }
                 });
     }
+
+    @Override
+    public void onBackPressed() {
+        return;
+    }
 }
